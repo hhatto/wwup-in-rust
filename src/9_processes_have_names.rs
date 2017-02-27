b96be8cab979f74500a3084202a84f5b1e6245cb
hhatto <hhatto.jp@gmail.com>
2017-02-28 01:04:43 +0900

    add sleep

--- src/9_processes_have_names.rs
+++ src/9_processes_have_names.rs
@@ -1,5 +1,9 @@
 extern crate prctl;
 
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    prctl::set_name("wwup-in-rust process");
+    let _ = prctl::set_name("wwupproc");
+    thread::sleep(Duration::from_millis(10_000));   // show with top command!!
 }
