772d4fc5b2e60ca5799286cad1b95f897765352c
Hideo Hattori <hattori-h@klab.com>
2017-03-04 20:47:00 +0900

    execute only target_os is linux

--- src/9_processes_have_names.rs
+++ src/9_processes_have_names.rs
@@ -1,9 +1,20 @@
 extern crate prctl;
 
+#[cfg(target_os="linux")]
 use std::thread;
+#[cfg(target_os="linux")]
 use std::time::Duration;
 
-fn main() {
+#[cfg(target_os="linux")]
+fn _main() {
     let _ = prctl::set_name("wwupproc");
     thread::sleep(Duration::from_millis(10_000));   // show with top command!!
+}
+
+#[cfg(not(target_os="linux"))]
+fn _main() {
+}
+
+fn main() {
+    _main();
 }
