a4f54c80b129127ec73e47133bc9217fa8fb8c76
Hideo Hattori <hattori-h@klab.com>
2017-03-11 16:01:49 +0900

    12

---
+++ src/12_orphaned_processes.rs
@@ -0,0 +1,21 @@
+extern crate nix;
+
+use std::process;
+use nix::unistd;
+
+fn main() {
+    let pid = unistd::getpid();
+    println!("pid={}", pid);
+
+    match unistd::fork().expect("fork() error") {
+        unistd::ForkResult::Parent{ child } => {
+            process::exit(0);
+        }
+        unistd::ForkResult::Child => {
+            for _ in 0..5 {
+                unistd::sleep(1);
+                println!("I'm an orphan!");
+            }
+        }
+    }
+}
