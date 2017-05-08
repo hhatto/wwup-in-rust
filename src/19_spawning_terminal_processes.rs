7ef78053e5a0447048fafc7824dd7adb64c60c12
Hideo Hattori <hattori-h@klab.com>
2017-05-08 21:46:49 +0900

    19

---
+++ src/19_spawning_terminal_processes.rs
@@ -0,0 +1,29 @@
+extern crate libc;
+
+use std::process::Command;
+use std::ffi::CString;
+
+fn process_spawn() {
+    let mut sleep_command = Command::new("sleep").arg("3").spawn().expect("sleep command error");
+    let mut ls_command = Command::new("ls").arg("-l").spawn().expect("ls command error");
+
+    let ret = sleep_command.wait().expect("sleep wait error");
+    println!("sleep result={}", ret);
+    let ret = ls_command.wait().expect("ls wait error");
+    println!("ls result={}", ret);
+}
+
+fn libc_popen() {
+    let msg = CString::new("some\ndata").unwrap();
+    unsafe {
+        let file = libc::popen(CString::new("less").unwrap().as_ptr(),
+                    CString::new("w").unwrap().as_ptr());
+        libc::fwrite(msg.as_ptr() as *const libc::c_void, 1, msg.to_bytes().len(), file);
+        libc::pclose(file);     // exec less -> input q -> process end
+    };
+}
+
+fn main() {
+    process_spawn();
+    libc_popen();
+}
